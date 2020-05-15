4b2e5775df139cfaf3a8372797bdd1ac8188d926
Michaelle Barbor <michaelle.barbor@outlook.com>
2020-05-15 03:30:32 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+  # my-collect(collection) << yield(array[i])
+  #   # yield(array[i])
+    i += 1
+  end
+  collection
+end
  
 
